056b1b0e9db0a8040cdc661eeed73655db5bd28e
erfaenda <regalko7@gmail.com>
2017-12-07 16:14:33 +0500

    Добавьте файлы проекта.

---
+++ StudySharp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudySharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hellow world!");
+            Calculator.Add(10, 10);
+            Console.ReadKey();
+        }
+    }
+    class Calculator
+    {
+        public static void Add(int x, int y)
+        {
+            int z = x + y;
+            Console.WriteLine($"Сумма {x} и {y} равна {z}");
+        }
+    }
+        
+        
+
+
+
+}
